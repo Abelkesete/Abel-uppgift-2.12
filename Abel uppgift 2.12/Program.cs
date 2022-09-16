255f1758c36f397aee10750bfe91fc8f358d7ae0
Pingvu <89245290+BaoLanNguyen@users.noreply.github.com>
2022-09-16 14:20:51 +0200

    Add project files.

---
+++ Abel uppgift 2.12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift_2._12
+{
+    class program
+    {
+        static void Main()
+        {
+            int startavgift = 300;
+            Console.WriteLine("Hur många dagar vill du hyra bilen?");
+            int dagar = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många kilometer ska du köra?");
+            int kilometer = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(startavgift + dagar * 500 - 500 + kilometer);
+
+        }
+    }
+}
